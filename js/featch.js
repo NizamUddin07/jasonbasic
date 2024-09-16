58ac30ae272ba7f03da39b91e5bfb7e88b61e8b1
MD NIZAM UDDIN <53169290+NizamUddin07@users.noreply.github.com>
2024-09-16 22:39:08 +0600

    get data from jason from

--- js/featch.js
+++ js/featch.js
@@ -1,3 +1,23 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+function loadData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url).then(Response=> Response.json())
+    .then(json=>console.log(json))
+}
+
+function loadUser(){
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch  (url).then(Response=>Response.json()).then(json=>console.log(json))
+}
+
+
+
+function displayUser2(data){
+    for(const user of data){
+        console.log(user.email);
+    }
+}
